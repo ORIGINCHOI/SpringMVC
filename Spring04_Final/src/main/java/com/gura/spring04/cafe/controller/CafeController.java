2576219e8f9001a1990110360cca66dd517cbc03
Origin Choi <kyun4525@gmail.com>
2022-12-23 15:28:07 +0900

    Spring04 에 글 수정기능 구현함

--- Spring04_Final/src/main/java/com/gura/spring04/cafe/controller/CafeController.java
+++ Spring04_Final/src/main/java/com/gura/spring04/cafe/controller/CafeController.java
@@ -49,4 +49,16 @@ public class CafeController {
        service.deleteContent(num, request);
        return "redirect:/cafe/list";
     }
+    
+    @RequestMapping("/cafe/updateform")
+    public String updateForm(HttpServletRequest request) {
+    	service.getData(request);
+    	return "cafe/updateform";
+    }
+    
+    @RequestMapping("/cafe/update")
+    public String update(CafeDto dto) {
+    	service.updateContent(dto);
+    	return "cafe/update";
+    }
 }
