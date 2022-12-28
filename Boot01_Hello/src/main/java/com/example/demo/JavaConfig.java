df8d24afe8ff87e88dfb190bf4006e70166c72e3
Origin Choi <kyun4525@gmail.com>
2022-12-28 15:57:02 +0900

    Boot01 에 주석 추가함

--- Boot01_Hello/src/main/java/com/example/demo/JavaConfig.java
+++ Boot01_Hello/src/main/java/com/example/demo/JavaConfig.java
@@ -4,14 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /*
- *  어떤 객체를 spring 이 생성해서 관리할지 설정
+ *  어떤 객체를 spring 이 생성해서 관리할지 설정( bean 설정 )
  */
 @Configuration
 public class JavaConfig {
 	
 	//이 메소드에서 리턴되는 객체를 spring 이 관리하는 bean 이 되도록 한다. 
+	//아래의 메소드는 xml 문서에서 <bean id="myCar" class="com.example.demo.Car" /> 와 같다
 	@Bean
-	public Car myCar() {
+	public Car myCar() { //method 의 이름이 bean 의 이름 역할을 한다.
 		System.out.println("myCar 메소드 호출됨");
 		Car c1=new Car();
 		return c1;
